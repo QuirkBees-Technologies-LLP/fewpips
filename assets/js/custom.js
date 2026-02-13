e41b382ff57dfa64c4af125b53912bf2a4fe853a
vaholiyaakshit <akshitvaholiya@gmail.com>
2026-02-13 19:43:07 +0530

    design change in all section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -119,27 +119,23 @@ function updateRange() {
 // Initial update
 updateRange();
 
-const tabs = document.querySelectorAll(".plan-tab");
-const contents = document.querySelectorAll(".tab-content");
-
-tabs.forEach(tab=>{
-  tab.addEventListener("click",()=>{
-
-    // active tab remove
-    tabs.forEach(t=>t.classList.remove("active"));
-    tab.classList.add("active");
-
-    // content hide
-    contents.forEach(c=>c.classList.remove("active"));
-
-    // content show
-    const id = tab.getAttribute("data-tab");
-    document.getElementById(id).classList.add("active");
-
-  });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-  const list = document.querySelector(".payout-scroll ul");
-  list.innerHTML += list.innerHTML; // duplicate items
+    const list = document.querySelector(".payout-scroll ul");
+    list.innerHTML += list.innerHTML; // duplicate items
 });
+
+
+
+const cards = document.querySelectorAll('.why-card');
+
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show-card');
+    }
+  });
+},{threshold:.18});
+
+cards.forEach(card=>observer.observe(card));
+
+
